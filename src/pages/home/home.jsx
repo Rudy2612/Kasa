e22fcb7e498d613e4f530c24525816f3119c018a
Rudy <Rudy.26.fr@gmail.com>
2024-04-09 23:02:07 +0200

    added banner to homepage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,9 @@ export default function Home() {
 
             <div className="home__banner">
                 <img src={banner} alt="banner"></img>
-                <h1></h1>
+                <div className="home__banner__text">
+                    <h1>Chez vous, partout et ailleurs</h1>
+                </div>
             </div>
         </div>
     )
