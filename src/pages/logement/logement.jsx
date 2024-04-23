6000c1670da2b3259c827fb8182a121dae606940
Rudy <Rudy.26.fr@gmail.com>
2024-04-23 16:56:03 +0200

    add page 'Logement'

---
+++ src/pages/logement/logement.jsx
@@ -0,0 +1,44 @@
+import logements from "../../assets/logements.json"
+import { useParams } from "react-router-dom"
+import Carroussel from "../../components/carrousel";
+import Tags from "../../components/tags";
+import Host from "../../components/host";
+import Rating from "../../components/rating";
+import Dropdown from "../../components/dropdown/dropdown";
+
+
+
+export default function Logement() {
+    let { id } = useParams();
+    let logement = logements.find((element) => element.id === id)
+    console.log(logement)
+
+    return (
+        <div className="logement">
+            <Carroussel pictures={logement.pictures} title={logement.title} />
+
+            <div className="logement__wrap">
+
+                <div>
+                    <div className="logement__title">
+                        <h1>{logement.title}</h1>
+                        <p>{logement.location}</p>
+                    </div>
+
+                    <Tags tags={logement.tags} />
+                </div>
+
+                <div className="logement__host-wrap">
+                    <Host host={logement.host} />
+                    <Rating rating={logement.rating} />
+                </div>
+
+            </div>
+
+            <div className="logement__details-wrap">
+                <Dropdown title={'Description'} content={logement.description} />
+                <Dropdown title={'Équipements'} content={logement.equipments} />
+            </div>
+        </div>
+    )
+}
