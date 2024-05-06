844e5ed4186ae740388ac8616d673217538208f3
Rudy <Rudy.26.fr@gmail.com>
2024-05-06 16:57:41 +0200

    banner component created

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,17 +1,13 @@
 import banner from "../../assets/banner.png"
-import ThumbnailGrid from "../../components/thumbnail_grid/thumbnail_grid"
+import Banner from "../../components/banner"
+import ThumbnailGrid from "../../components/thumbnail_grid"
 
 export default function Home() {
 
     return (
         <div className="home">
 
-            <div className="home__banner">
-                <img src={banner} alt="banner"></img>
-                <div className="home__banner__text">
-                    <h1>Chez vous, partout et ailleurs</h1>
-                </div>
-            </div>
+            <Banner img={banner} text={"Chez vous, partout et ailleurs"} />
 
             <div>
                 <ThumbnailGrid />
