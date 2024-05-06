fd4de63f8606752fbf71fec11a1806bf41680a2b
Rudy <Rudy.26.fr@gmail.com>
2024-05-06 16:54:42 +0200

    add condition to arrow

--- src/components/carrousel/index.jsx
+++ src/components/carrousel/index.jsx
@@ -22,18 +22,17 @@ export default function Carroussel({ pictures = [], title }) {
 
             <img src={pictures[index]} alt={title}></img>
 
-            <div className="carrousel__arrow-left" onClick={() => handleChange(1)}>
+            {pictures.length >= 2 && <div className="carrousel__arrow-left" onClick={() => handleChange(1)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
                     <path d="M47.0399 7.42497L39.9199 0.344971L0.359863 39.945L39.9599 79.545L47.0399 72.465L14.5199 39.945L47.0399 7.42497Z" fill="white" />
                 </svg>
-            </div>
+            </div>}
 
-            <div className="carrousel__arrow-right" onClick={() => handleChange(-1)}>
+            {pictures.length >= 2 && <div className="carrousel__arrow-right" onClick={() => handleChange(-1)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
                     <path d="M0.959961 72.9877L8.03996 80.0677L47.64 40.4677L8.03996 0.867676L0.959961 7.94768L33.48 40.4677L0.959961 72.9877Z" fill="white" />
                 </svg>
-            </div>
-
+            </div>}
 
         </div>
     )
