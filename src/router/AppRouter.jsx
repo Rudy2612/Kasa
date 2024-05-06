a02cc404d69385565e81ccf3606ca21bf58ac6eb
Rudy <Rudy.26.fr@gmail.com>
2024-05-06 16:58:00 +0200

    created all routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "../components/header/header";
+import Header from "../components/header";
 import Home from "../pages/home/home";
+import Footer from "../components/footer";
+import Logement from "../pages/logement/logement";
+import Error from "../pages/error/error";
+import About from "../pages/about/about";
 
 export default function AppRouter() {
     return (
@@ -8,10 +12,11 @@ export default function AppRouter() {
             <Header />
             <Routes>
                 <Route index element={<Home />} />
-                <Route path="logement/:id" element={<>Logement</>} />
-                <Route path="a-propos" element={<>À-propos</>} />
-                <Route path="*" element={<>Error</>} />
+                <Route path="logement/:id" element={<Logement />} />
+                <Route path="a-propos" element={<About />} />
+                <Route path="*" element={<Error />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     )
 }
