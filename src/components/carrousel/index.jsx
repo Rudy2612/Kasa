7ce80f9cf25f8698e14f3b06c94608fdbf8ea2c3
Rudy <Rudy.26.fr@gmail.com>
2024-06-06 16:53:33 +0200

    replace <svg> by FontAwesomeIcon

--- src/components/carrousel/index.jsx
+++ src/components/carrousel/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 export default function Carroussel({ pictures = [], title }) {
     let [index, setIndex] = useState(0);
@@ -23,15 +25,11 @@ export default function Carroussel({ pictures = [], title }) {
             <img src={pictures[index]} alt={title}></img>
 
             {pictures.length >= 2 && <div className="carrousel__arrow-left" onClick={() => handleChange(1)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
-                    <path d="M47.0399 7.42497L39.9199 0.344971L0.359863 39.945L39.9599 79.545L47.0399 72.465L14.5199 39.945L47.0399 7.42497Z" fill="white" />
-                </svg>
+                <FontAwesomeIcon icon={faChevronLeft} />
             </div>}
 
             {pictures.length >= 2 && <div className="carrousel__arrow-right" onClick={() => handleChange(-1)}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
-                    <path d="M0.959961 72.9877L8.03996 80.0677L47.64 40.4677L8.03996 0.867676L0.959961 7.94768L33.48 40.4677L0.959961 72.9877Z" fill="white" />
-                </svg>
+                <FontAwesomeIcon icon={faChevronRight} />
             </div>}
 
         </div>
--- src/components/dropdown/index.jsx
+++ src/components/dropdown/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import PropTypes from 'prop-types';
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Dropdown({ title, content }) {
 
@@ -17,9 +19,7 @@ export default function Dropdown({ title, content }) {
             <div className="dropdown__topbar">
                 <span className="dropdown__title">{title}</span>
 
-                <svg onClick={handleOpen} className={`dropdown__chevron ${isOpen ? "open" : "close"}`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                    <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z" />
-                </svg>
+                <FontAwesomeIcon onClick={handleOpen} className={`dropdown__chevron ${isOpen ? "open" : "close"}`} icon={faChevronUp} />
             </div>
 
 
